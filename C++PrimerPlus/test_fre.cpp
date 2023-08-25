bb314b529970d5c5f2ab068fd5e421331bb1ecde
stlrcp <109725151+stlrcp@users.noreply.github.com>
2023-08-25 16:24:31 +0800

    Update test_fre.cpp
    update

--- C++PrimerPlus/test_fre.cpp
+++ C++PrimerPlus/test_fre.cpp
@@ -260,4 +260,45 @@ int main() {
     obj.printValue();
     return 0;
 }
+
+
+#include <iostream>
+using namespace std;
+class Box
+{
+    public:
+        // 构造函数定义
+        Box(double l=2.0, double b=2.0, double h=2.0)
+        {
+            cout << "调用构造函数：" << endl;
+            length = l;
+            breadth = b;
+            height = h;
+        }
+        double Volume()
+        {
+            return length * breadth * height;
+        }
+        int compare(Box box)
+        {
+            return this->Volume() > box.Volume();
+        }
+    private:
+        double length; 
+        double breadth;
+        double height;
+};
+int main()
+{
+    Box Box1(3.3, 1.2, 1.5);
+    Box Box2(8.5, 6.0, 2.0);
+    if (Box1.compare(Box2))
+    {
+        cout << "Box2 的体积比 Box1 小 " << endl;
+    } else {
+        cout << "Box2 的体积大于或等于 Box1 " << endl;
+    }
+    return 0;
+}
 */
+
