e5e1a9b3c23f7dac649b12a64847b54ba7f65fbc
stlrcp <109725151+stlrcp@users.noreply.github.com>
2023-09-04 19:07:14 +0800

    Update test_fre.cpp
    update

--- C++PrimerPlus/test_fre.cpp
+++ C++PrimerPlus/test_fre.cpp
@@ -927,6 +927,50 @@ int main(int argc, char* argv[])
         cout << "B::n = " << b.getB() << endl;
         return 0;
 }
+
+
+// 构造函数调用顺序：基类 > 成员类 > 派生类；
+// 多继承派生类：基类构造顺序 依据 基类继承顺序调用
+// 类成员：依据类成员对象 定义顺序 调用成员类构造函数
+#include <iostream>
+using namespace std;
+class Shape{    // 基类  Shape
+    public:
+        Shape() {
+            cout << "Shape" << endl;
+        }
+        ~Shape() {
+            cout << "~Shape()" << endl;
+        }
+};
+
+class PaintCost{   // 基类 PaintCost
+    public:
+        PaintCost() {
+            cout << "PaintCost" << endl;
+        }
+        ~PaintCost() {
+            cout << "~PaintCost" << endl;
+        }
+};
+// 派生类
+class Rectangle : public Shape, public PaintCost  // 基类构造顺序 依照 继承顺序
+{
+    public:
+        Rectangle(): b(), a(), Shape(), PaintCost(){
+            cout << "Rectangle" << endl;
+        }
+        ~Rectangle() {
+            cout << "~Rectangle" << endl;
+        }
+        PaintCost b;    // 类成员变量构造顺序 依据 变量定义顺序
+        Shape a;
+};
+int main(void)
+{
+        Rectangle Rect;
+        return 0;
+}
 */
 
 
