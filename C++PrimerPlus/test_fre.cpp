61c4a9016ca63ecf83a8f419777eec9d6e798226
stlrcp <109725151+stlrcp@users.noreply.github.com>
2023-09-15 18:36:18 +0800

    Update test_fre.cpp
    update

--- C++PrimerPlus/test_fre.cpp
+++ C++PrimerPlus/test_fre.cpp
@@ -1549,6 +1549,74 @@ int main(){
         a5.display();
         return 0;
 }
+
+
+// 对实例进行改写，以非成员函数的方式重载运算符 +
+#include <iostream>
+using namespace std;
+class Box{
+    double length;    // 长度
+    double breadth;   // 宽度
+    double height;   // 高度
+    public:
+        double getVolume(void){
+            return length * breadth * height;
+        }
+
+        void setLength(double len){
+            length = len;
+        }
+
+        void setBreadth(double bre){
+            breadth = bre;
+        }
+
+        void setHeight(double hei){
+            height = hei;
+        }
+        // 重载 + 运算符，用于把两个 Box 对象相加，因为其是全局函数，对应的参数个数是2，当重载的运算符函数是全局函数时，需要在类中将该函数声明为友元
+        friend Box operator+(const Box &a, const Box &b);
+};
+Box operator+(const Box& a, const Box& b){
+        Box box;
+        box.length = a.length + b.length;
+        box.breadth = a.breadth + b.breadth;
+        box.height = a.height + b.height;
+        cout << box.length << "--" << box.breadth << "--" << box.height << endl;
+        return box;
+}
+// 程序的主函数
+int main() {
+        Box Box1;    // 声明 Box1， 类型为 Box
+        Box Box2;
+        Box Box3;
+        double volume = 0.0;   // 把体积存储在该变量中
+        // Box1 详述
+        Box1.setLength(6.0);
+        Box1.setBreadth(7.0);
+        Box1.setHeight(5.0);
+
+        // Box2 详述
+        Box2.setLength(12.0);
+        Box2.setBreadth(13.0);
+        Box2.setHeight(10.0);
+
+        // Box1 的体积
+        volume = Box1.getVolume();
+        cout << "Volume of Box1 : " << volume << endl;
+
+        // Box2 的体积
+        volume = Box2.getVolume();
+        cout << "Volume of Box2 : " << volume << endl;
+
+        // 把两个对象相加，得到 Box3
+        Box3 = Box1 + Box2;
+        // Box3 的体积
+        volume = Box3.getVolume();
+        cout << "Volume of Box3 : " << volume << endl;
+
+        return 0;
+}
 */
 
 
