49dcfb37483142c2868b985dd2e40fb2b29324ce
stlrcp <109725151+stlrcp@users.noreply.github.com>
2024-06-28 18:36:56 +0800

    Create main.cpp
    update

---
+++ C++PrimerPlus/Cmake_test/share_tmp/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/libexample.h"
+
+int main(){
+    int result = add(3, 4);
+    std::cout << "Result of add(3, 4): " << result << std::endl;
+    return 0;
+}
